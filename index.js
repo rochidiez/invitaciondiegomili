8a688505c372291a7019d2e8945c882fe0c94a6d
Rochi Diez <rochi@tiendanube.com>
2018-03-24 19:41:14 -0300

    pictures

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-var cool = require('cool-ascii-faces');
 var express = require('express');
 var app = express();
 
@@ -14,8 +13,8 @@ app.get('/', function(request, response) {
   response.render('pages/index')
 });
 
-app.get('/cool', function(request, response) {
-  response.send(cool());
+app.get('/gifts', function(request, response) {
+    response.render('pages/gifts')
 });
 
 app.listen(app.get('port'), function() {
