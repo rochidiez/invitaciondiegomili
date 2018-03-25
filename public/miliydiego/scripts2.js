3468da4402e7551606f57c6f5cd5a69471d0b6a1
Rochi Diez <rochi@tiendanube.com>
2018-03-25 20:08:06 -0300

    new script

---
+++ public/miliydiego/scripts2.js
@@ -0,0 +1,38 @@
+$(document).ready(function() { 
+    // Append .background-image-holder <img>'s as CSS backgrounds
+
+    $('.background-image-holder').each(function() {
+        var imgSrc = $(this).children('img').attr('src');
+        $(this).css('background', 'url("' + imgSrc + '")');
+        $(this).children('img').hide();
+        $(this).css('background-position', 'initial');
+    });
+
+    // Fade in background images
+
+    setTimeout(function() {
+        $('.background-image-holder').each(function() {
+            $(this).addClass('fadeIn');
+        });
+    }, 200);
+    
+    // Countdown Timers
+
+    if ($('.countdown').length) {
+        $('.countdown').each(function() {
+            var date = $(this).attr('data-date');
+            $(this).countdown(date, function(event) {
+                $(this).text(
+                    event.strftime('Faltan %D días %H:%M:%S')
+                );
+            });
+        });
+    }
+    
+    // Disable parallax on mobile
+
+    if ((/Android|iPhone|iPad|iPod|BlackBerry|Windows Phone/i).test(navigator.userAgent || navigator.vendor || window.opera)) {
+        $('section').removeClass('parallax');
+    }
+
+});
